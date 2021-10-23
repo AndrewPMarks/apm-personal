cb5efb3921699bc80afe09832fd995dea35b671d
Andrew Marks <andrewpmarks@outlook.com>
2021-10-23 02:50:27 -0600

    Created react project and added firebase configuration

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.scss'
+import { firebaseInit } from './firebase'
+
+export const App = () => {
+	firebaseInit()
+
+	return <div className='App'>Under construction</div>
+}
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+
+export const firebaseInit = () => {
+	const firebaseConfig = {
+		apiKey: 'AIzaSyC3CIVOMdZgB-X_oUqZKMU51YYO0NF_vKo',
+		authDomain: 'apm-personal.firebaseapp.com',
+		projectId: 'apm-personal',
+		storageBucket: 'apm-personal.appspot.com',
+		messagingSenderId: '643343842108',
+		appId: '1:643343842108:web:ef6554fee89aa875aefc39',
+		measurementId: 'G-D9VB1HX28W',
+	}
+
+	const app = initializeApp(firebaseConfig)
+	const analytics = getAnalytics(app)
+
+	return [app, analytics]
+}
