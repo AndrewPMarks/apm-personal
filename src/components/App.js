2f10428a0d7fa1d35ab54d44d603fea8eeec53d6
Andrew Marks <andrewpmarks@outlook.com>
2021-10-23 19:00:28 -0600

    Added nav and hero transitions based on website pathname

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import '../styles/App.scss'
 import { firebaseInit } from '../firebase'
+import { Header } from './Header'
+import { Switch, Route } from 'react-router-dom'
 
 export const App = () => {
     firebaseInit()
 
-    return <div className='App'>Under construction</div>
+    return (
+        <div className='App'>
+            <Header></Header>
+            <Switch>
+                <Route path='/projects'>Projects</Route>
+                <Route path='/'>Home</Route>
+            </Switch>
+        </div>
+    )
 }
